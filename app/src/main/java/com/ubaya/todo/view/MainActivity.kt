6dfa945766ccdccb7e03537ae15a700e9371a833
andreas <s160419047@student.ubaya.ac.id>
2022-05-11 21:38:53 +0700

    Update until week 9 exercise

---
+++ app/src/main/java/com/ubaya/todo/view/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.ubaya.todo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.ubaya.todo.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController:NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragmentHost)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
---
+++ app/src/main/java/com/ubaya/todo/view/TodoListAdapter.kt
@@ -0,0 +1,47 @@
+package com.ubaya.todo.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.ubaya.todo.R
+import com.ubaya.todo.model.Todo
+import java.util.ArrayList
+import kotlinx.android.synthetic.main.todo_item_layout.view.*
+
+class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick : (Todo) -> Unit)
+    :RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
+    class TodoViewHolder(var view:View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.todo_item_layout, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int)
+    {
+        holder.view.checkBox.text = todoList[position].title
+        holder.view.checkBox.setOnCheckedChangeListener { compoundButton, b ->
+            adapterOnClick(todoList[position])
+        }
+        holder.view.imgEdit.setOnClickListener {
+            val action = TodoListFragmentDirections.actionEditTodo(todoList[position].uuid)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        holder.view.checkBox.setOnCheckedChangeListener { compoundButton, b ->
+            if(b == true) adapterOnClick(todoList[position])
+        }
+    }
+
+    override fun getItemCount(): Int = todoList.size
+
+    fun updateTodoList(newTodoList: List<Todo>) {
+        todoList.clear()
+        todoList.addAll(newTodoList)
+        notifyDataSetChanged()
+    }
+
+}
